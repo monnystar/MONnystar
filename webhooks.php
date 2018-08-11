570c8a4d1359ed3d6fff8100152994a5e91aef05
monnystar <42146964+monnystar@users.noreply.github.com>
2018-08-11 21:14:29 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -19,17 +19,22 @@ if (!is_null($events['events'])) {
     // Reply only when message sent is in 'text' format
     if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
       
-      $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
-      $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
-      $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("$com");
-      $response = $bot->pushMessage($idPush, $textMessageBuilder);
-            
-      $Topic = "NodeMCU1";
-      $lineMsg = "$com";
-      getMqttfromlineMsg($Topic,$lineMsg);
-     }
-    else {
-
+         $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+         $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+         $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("$com");
+         $response = $bot->pushMessage($idPush, $textMessageBuilder);
+      
+      if($com == "เปิดไฟ"){      
+         $Topic = "NodeMCU1";
+         $lineMsg = "1";
+         getMqttfromlineMsg($Topic,$lineMsg);
+      }      
+        else if($com == "ปิดไฟ")         
+         $Topic = "NodeMCU1";
+         $lineMsg = "0";
+         getMqttfromlineMsg($Topic,$lineMsg);
+      }
+  }else {
      $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
       $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
       $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('1');
