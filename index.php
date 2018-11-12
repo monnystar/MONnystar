3c7648f8f00019779673ab1afee4c6100fb9cca1
monnystar <42146964+monnystar@users.noreply.github.com>
2018-11-12 16:26:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,54 @@
 <?php
+require "vendor/autoload.php";
+
 if(name == samickrock3 && val == 50){
-echo "in IF Hello LINE BOT";
+  	$Topic = "NodeMCU1";
+		$lineMsg = "in IF Hello LINE BOT";
+		getMqttfromlineMsg($Topic,$lineMsg);	
+   echo "in IF Hello LINE BOT";
 }
 else{
+  	$Topic = "NodeMCU1";
+		$lineMsg =  "in else Hello LINE BOT";
+		getMqttfromlineMsg($Topic,$lineMsg);	
+   echo "in IF Hello LINE BOT";
 echo "in else Hello LINE BOT";
 }
+
+function pubMqtt($topic,$msg){
+   $APPID= "samickrock/";
+   $KEY = "MC6kLl4SYiDW2qd";
+   $SECRET = "ASn4eO61s65RPZ3ujHSHNulOz"; 
+   $Topic = "$topic"; 
+   put("https://api.netpie.io/microgear/".$APPID.$Topic."?retain&auth=".$KEY.":".$SECRET,$msg);
+   
+  }
+ function getMqttfromlineMsg($Topic,$lineMsg){
+ 
+    $pos = strpos($lineMsg, ":");
+    if($pos){
+      $splitMsg = explode(":", $lineMsg);
+      $topic = $splitMsg[0];
+      $msg = $splitMsg[1];
+      pubMqtt($topic,$msg);
+    }else{
+      $topic = $Topic;
+      $msg = $lineMsg;
+      pubMqtt($topic,$msg);
+    }
+  }
+function put($url,$tmsg)
+{ 
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);   
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);  
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT"); 
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $tmsg);
+    curl_setopt($ch, CURLOPT_USERPWD, "MC6kLl4SYiDW2qd:ASn4eO61s65RPZ3ujHSHNulOz");     
+    $response = curl_exec($ch);
+    curl_close($ch);
+    echo $response . "\r\n";
+    return $response;
+}  
+ ?>
