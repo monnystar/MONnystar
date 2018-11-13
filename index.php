735c8c51d6fc877e87b9667e20567c4fbb71e1d5
monnystar <42146964+monnystar@users.noreply.github.com>
2018-11-13 17:54:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $com = substr($content, 274, -5);
 $textmessagerobot = str_replace(" ","",$com);
 $i = 0;
   $Topic = "NodeMCU1";
-  $lineMsg = "zxvzxv"+"$com";
+  $lineMsg = "zxvzxv"+"$content";
    getMqttfromlineMsg($Topic,$lineMsg);
 /*
 if (!is_null($events['events'])) {
