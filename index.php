f818b593abdf89160291e0bb0ba0a7d4b1614259
monnystar <42146964+monnystar@users.noreply.github.com>
2019-02-02 00:13:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -78,7 +78,7 @@ if($test5 <= 15){
  		if(date("H:i") >= "16:30" || date("H:i") <= "8:30"){
 			notify_message("เวลา 16.30 มโนยกเลิกการทำงาน",$token);
  		}
-		  else {
+		  else {		  
 			  if($test4 > 1){
 				 $Topic = "NodeMCU1";
 				 $lineMsg = "codeA".$roomnumber;
@@ -109,8 +109,7 @@ if($test5 <= 15){
 				$Topic = "NodeMCU1";
 				$lineMsg = "codeP";
 				getMqttfromlineMsg($Topic,$lineMsg);	
-			  }
-	 
+			  }}
 	}
       }
     }
