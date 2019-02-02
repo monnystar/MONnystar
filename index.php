e3100008b22d3bcb6a09438c46e51829dbbb048d
monnystar <42146964+monnystar@users.noreply.github.com>
2019-02-02 16:09:43 +0700

    Update index.php

--- index.php
+++ index.php
@@ -75,19 +75,19 @@ if($test5 <= 15){
 	 foreach ($events['events'] as $event) {
 	  if ($event['type'] == 'message' && $event['message']['type'] == 'text'){		
 		 if($test4 > 1){
-			date_default_timezone_set("Asia/Bangkok");
- 			if(date("H:i") >= "16:30" || date("H:i") <= "8:30"){		
-				notify_message("เวลา 16.30น. มโนยกเลิกการทำงาน\nและเปิดใช้งานอีกครั้ง 8.30น.",$token);		
- 			} else {
-				for($i = 0;$i <= 5;$i++){
-				if($roomnumber[$i] >= 5){
-				notify_message("หม้ายเลขห้องผิดพลาดกรุณาส่งคำสั่งมาใหม่\nหม้ายเลขห้องคือ 1 2 3 4",$token);
-				}else{						
+			//date_default_timezone_set("Asia/Bangkok");
+ 			//if(date("H:i") >= "16:30" || date("H:i") <= "8:30"){		
+			//	notify_message("เวลา 16.30น. มโนยกเลิกการทำงาน\nและเปิดใช้งานอีกครั้ง 8.30น.",$token);		
+ 			//} else {
+			//	for($i = 0;$i <= 5;$i++){
+			//	if($roomnumber[$i] >= 5){
+		//		notify_message("หม้ายเลขห้องผิดพลาดกรุณาส่งคำสั่งมาใหม่\nหม้ายเลขห้องคือ 1 2 3 4",$token);
+		//		}else{						
 				$Topic = "NodeMCU1";
 				$lineMsg = "codeA".$roomnumber;
 				getMqttfromlineMsg($Topic,$lineMsg);	   
-			  }
-		        }
+		//	  }
+		//        }
 		      }
 		    }
 		else if($test4 < 1 ){
